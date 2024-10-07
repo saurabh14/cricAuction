a3d4f36ae3e60437df4299793a9c2c0bbea3f221
saurabh14 <gsaurabh1407@gmail.com>
2024-10-08 00:14:17 +0530

    adding auction page

---
+++ src/components/auctionPage/PlayerCard.jsx
@@ -0,0 +1,12 @@
+import PlayerInfo from "./PlayerInfo";
+
+
+function PlayerCard(){
+    return(
+        <div className="PlayerCard">
+            <PlayerInfo />
+        </div>
+    )
+}
+
+export default PlayerCard;
---
+++ src/components/auctionPage/PlayerInfo.jsx
@@ -0,0 +1,16 @@
+function PlayerInfo(){
+    return(
+            <>
+            <div className="PlayerCard-img"></div>
+            <div className="PlayerCard-name">Saurabh Gupta</div>
+            <div className="PlayerCard-info">
+                <div className="PlayerCardInfo-matches">4</div>
+                <div className="PlayerCardInfo-runs">224</div>
+                <div className="PlayerCardInfo-wickets">2</div>
+                <div className="PlayerCardInfo-lastTeam">C Sharp Kings</div>
+            </div>
+            </>
+    )
+}
+
+export default PlayerInfo;
